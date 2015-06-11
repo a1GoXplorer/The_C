34477eeac61919b3ac9f44ecb78738f5a281af0c
Mark Daniels <mark_daniels1982@hotmail.com>
2015-06-11 15:39:09 -0700

    file copying

--- temperatures.c
+++ temperatures.c
@@ -5,17 +5,17 @@
 
   int main()
   {
-      int fahr, celsius;
+      float fahr, celsius;
       int lower, upper, step;
 
-      lower = 0;  /* lower limit of temperature table */
-      upper = 300; /* upper limit */
-      step = 2; /* step size */
+         lower = 0;  /* lower limit of temperature table */
+         upper = 300; /* upper limit */
+         step = 2; /* step size */
 
       fahr = lower;
       while (fahr <= upper) {
-        celsius = 5 * (fahr - 32) / 9;
-        printf("%d\t%d\n", fahr, celsius);
+        celsius = (5.0 / 9.0) * (fahr - 32.0);
+        printf("%3.0f %6.1f\n", fahr, celsius);
         fahr = fahr + step;
       }
   }
